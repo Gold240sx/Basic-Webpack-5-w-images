6cedd4b220893a378b6b6b734b1d63dc5884b95e
Gold240sx <92943303+Gold240sx@users.noreply.github.com>
2022-01-06 05:50:16 -0600

    commit 0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { freedom } from 'js/functionality/freedom.js';
+
 console.log('hello from index');
+console.log(freedom);
+
+const firstImport = document.getElementById('firstImport');
+
+firstImport.innerText = freedom;
